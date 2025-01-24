83ec34413180e195d56399a4152872fa72af64c9
Balconsk <namaddo@gmail.com>
2025-01-24 19:46:19 +0500

    add camera and edit launch file in gazebo_launch

--- gazebo_launch/launch/robot_state_publisher.launch.py
+++ gazebo_launch/launch/robot_state_publisher.launch.py
@@ -7,6 +7,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 from launch.conditions import IfCondition
 from launch.substitutions import Command
 
+
 def generate_launch_description():
 
     ld = LaunchDescription()
@@ -14,23 +15,39 @@ def generate_launch_description():
     this_package = "gz_launch_tutorial"
     xacro_file = "my_bot.urdf.xacro"
 
-    urdf_package_arg = DeclareLaunchArgument(name = "urdf_package", default_value = this_package  )
+    urdf_package_arg = DeclareLaunchArgument(
+        name="urdf_package", default_value=this_package
+    )
 
     ld.add_action(urdf_package_arg)
 
-    urdf_package_path_arg = DeclareLaunchArgument(name = "path_to_urdf", 
-                                                  default_value = PathJoinSubstitution(["urdf", xacro_file]))
+    urdf_package_path_arg = DeclareLaunchArgument(
+        name="path_to_urdf", default_value=PathJoinSubstitution(["urdf", xacro_file])
+    )
 
     ld.add_action(urdf_package_path_arg)
 
-    urdf_path = PathJoinSubstitution([FindPackageShare(LaunchConfiguration('urdf_package')), LaunchConfiguration('path_to_urdf')])
+    urdf_path = PathJoinSubstitution(
+        [
+            FindPackageShare(LaunchConfiguration("urdf_package")),
+            LaunchConfiguration("path_to_urdf"),
+        ]
+    )
 
-    robot_description_content = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+    robot_description_content = ParameterValue(
+        Command(["xacro ", urdf_path]), value_type=str
+    )
 
-
-    ld.add_action(Node(package='robot_state_publisher',
-                       executable='robot_state_publisher',
-                       parameters=[{'robot_description': robot_description_content,}],
-                        ))
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[
+                {
+                    "robot_description": robot_description_content,
+                }
+            ],
+        )
+    )
 
     return ld
